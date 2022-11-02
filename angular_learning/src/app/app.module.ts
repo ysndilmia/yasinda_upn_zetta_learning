d60b301088b21df2055f9e3501e0cc7e78ad6553
ysndilmia <124200002@student.upnyk.ac.id>
2022-11-02 14:42:41 +0700

    day1

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModullComponent } from './modull/modull.component';
 import { Modul2Component } from './modul2/modul2.component';
+import { Modul1Component } from './modul1/modul1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ModullComponent,
-    Modul2Component
+    Modul2Component,
+    Modul1Component
   ],
   imports: [
     BrowserModule,
