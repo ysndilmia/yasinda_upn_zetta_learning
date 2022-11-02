45c035b72bd9c4304c49d8d751238ec06f5b16d6
ysndilmia <124200002@student.upnyk.ac.id>
2022-11-02 16:33:55 +0700

    ANGULAR MASTER

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ModullComponent } from './modull/modull.component';
-import { Modul2Component } from './modul2/modul2.component';
-import { Modul1Component } from './modul1/modul1.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ModullComponent,
-    Modul2Component,
-    Modul1Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
